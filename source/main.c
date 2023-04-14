a783d65ecee0ac490782bad8199e100aae77ac77
Gruetzig <florianavilov@gmail.com>
2023-04-14 23:25:43 +0200

    main.c fail

--- source/main.c
+++ source/main.c
@@ -1,26 +1,32 @@
 #include "ui.h"
 #include <3ds.h>
+#include "http.h"
 int main(int argc, char* argv[])
 {
     gfxInitDefault();
-	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
-	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
-    C2D_Prepare();
-    u32 color_white = C2D_Color32(0xFF, 0xFF, 0xFF, 0xFF);
-    C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
-    while (aptMainLoop()) {
-        hidScanInput();
-		u32 kDown = hidKeysDown();
-        if (kDown & KEY_START) break;
-        		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-        
-        C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-        C2D_TargetClear(top, C2D_Color32(0x00, 0x00, 0x00, 0xFF));
-        C2D_SceneBegin(top);
-        drawText(10, 10, 0, 1.0f, color_white, "Hello World", C2D_AlignLeft);
-        C3D_FrameEnd(0);
+    consoleInit(GFX_TOP, NULL);
+    initSocket();
+    int ret = 0;
+    char* site = "www.google.com";
+    char* path = "/";
+    char* response = "nope";
+    unsigned int len = 0;
+    ret = http_get(site, path, response, &len);
+    if (ret != -1) { 
+        printf("response: %s", response);
+    }else{
+        printf("fail lol %d", ret);
     }
-    C2D_Fini();
-	C3D_Fini();
-	gfxExit();
+    printf("nice");
+    while (aptMainLoop())
+    {
+        gspWaitForVBlank();
+        hidScanInput();
+
+        u32 kDown = hidKeysDown();
+        if (kDown & KEY_START)
+            break;
+    }
+    exitSocket();
+    gfxExit();
 }
